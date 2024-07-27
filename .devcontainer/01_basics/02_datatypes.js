fe6f2ced706a1adf162937a29ed12128861745b2
Aditi2799 <73766679+Aditi2799@users.noreply.github.com>
2024-07-27 09:05:53 +0000

    02

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let isLoggedIn= false
+let state;
+let name="Aditi"
+
+
+//primitive dt
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>(reactjs)unique
+
+
+//object()
+console.log(typeof "value");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
