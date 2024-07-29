5e136113b5b96afb04a0c96255c4542b80c40530
Aditi2799 <73766679+Aditi2799@users.noreply.github.com>
2024-07-29 06:20:14 +0000

    07

---
+++ .devcontainer/01_basics/06_strings.js
@@ -0,0 +1,20 @@
+const name="AditiPaikrao"
+const repoCount= 5
+//console.log(name + repoCount + "Value")=> don't use this
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+//=============functions================
+
+console.log(name.indexOf('t'))
+console.log(name.charAt(4))
+
+const newString= name.substring(0,3)
+console.log(newString)
+
+const anotherString=name.slice(-10,4)
+console.log(anotherString)
+
+const newString1="       ADITI          "
+console.log(newString1)
+console.log(newString1.trim())
---
+++ .devcontainer/01_basics/07_num_and_math.js
@@ -0,0 +1,22 @@
+const score=400
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNum=123.8999545
+console.log(otherNum.toPrecision(4))
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//==================MATHS===============
+console.log("--------------------------")
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min([4,3,554,5,6,321,12,154,5,456,]));
+
